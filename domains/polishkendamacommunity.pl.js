bda099362e2f95279c963b74c4d17c994c80b96d
kovansky <skowanski@f4dev.me>
2021-06-20 14:03:50 +0200

    Add robroyart.xyz domain records

--- domains/polishkendamacommunity.pl.js
+++ domains/polishkendamacommunity.pl.js
@@ -1,18 +1,23 @@
 D("polishkendamacommunity.pl", REGISTRAR_OVH, DnsProvider(PROVIDER_OVH),
     DefaultTTL(3600),
+
     MX('@', 1, 'mx1.mail.ovh.net.'),
     MX('@', 5, 'mx2.mail.ovh.net.'),
     MX('@', 100, 'mx3.mail.ovh.net.'),
+
     A('@', '151.80.140.22'),
     A('*', '151.80.140.22'),
     A('www', '151.80.140.22'),
-    CNAME('ftp', 'polishkendamacommunity.pl.'),
+
     TXT('@', '1|www.polishkendamacommunity.pl'),
     TXT('@', 'v=spf1 include:mx.ovh.com ~all'),
+    TXT('www', '3|welcome'),
+
+    CNAME('ftp', 'polishkendamacommunity.pl.'),
     CNAME('autoconfig', 'mailconfig.ovh.net.'),
     CNAME('autodiscover', 'mailconfig.ovh.net.'),
+
     SRV('_autodiscover._tcp', 0, 0, 443, 'mailconfig.ovh.net.'),
     SRV('_imaps._tcp', 0, 0, 993, 'ssl0.ovh.net.'),
-    SRV('_submission._tcp', 0, 0, 465, 'ssl0.ovh.net.'),
-    TXT('www', '3|welcome')
+    SRV('_submission._tcp', 0, 0, 465, 'ssl0.ovh.net.')
 )
--- providers.js
+++ providers.js
@@ -1,7 +1,8 @@
 // Registrars
 var REGISTRAR_NONE = NewRegistrar("none", "NONE")
+var REGISTRAR_NAMECHEAP = NewRegistrar("namecheap", "NAMECHEAP")
 var REGISTRAR_OVH = NewRegistrar("ovh", "OVH")
 
 // DNS Providers
-var PROVIDER_NAMECHEAP = NewDnsProvider("namecheap_sandbox", "NAMECHEAP")
+var PROVIDER_NAMECHEAP = NewDnsProvider("namecheap", "NAMECHEAP")
 var PROVIDER_OVH = NewDnsProvider("ovh", "OVH")
