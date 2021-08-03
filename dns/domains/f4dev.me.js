5b6f31e1c3ceecbdf84386a29b09099c8b76cb48
kovansky <skowanski@f4dev.me>
2021-08-03 16:27:09 +0200

    Disable CF proxy on outlook autodiscover

--- dns/domains/f4dev.me.js
+++ dns/domains/f4dev.me.js
@@ -9,12 +9,14 @@ D("f4dev.me", REGISTRAR_NONE, DnsProvider(PROVIDER_CLOUDFLARE),
     AAAA("platforma", "2001:41d0:301:5::27", CF_PROXY_ON),
     AAAA("www.platforma", "2001:41d0:301:5::27", CF_PROXY_ON),
 
+    // M365 configuration
     MX("@", 0, "f4dev-me.mail.protection.outlook.com."),
 
     CNAME("www", "kovansky.github.io.", CF_PROXY_ON), // Github Pages
-    CNAME("autodiscover", "autodiscover.outlook.com.", CF_PROXY_ON),
+    CNAME("autodiscover", "autodiscover.outlook.com.", CF_PROXY_OFF), // M365 Autodiscover
     CNAME("plugincontroller", "hosting.gitbook.io.", CF_PROXY_OFF), // Plugincontroller documentation
 
+    // M365 configuration
     TXT("default._domainkey", "v=DKIM1; k=rsa; s=email; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+kqPUJ7zu9Cjl/XOEaE7IGh+1f70Xelv0xwS5eQYh9ZQZdsfs74cc3lxgFr66ysDyD6wV41qyi/8r5A17HWMSiZ2KC6yqohPXh37J/DRIaw4Pfxcl2CAdxWeQFqtqoLvzf5AX1qFzoOz8oqPb0OpPs2TGMPyvnpYSUC1jcvGTDQIDAQAB"),
     TXT("_dmarc", "v=DMARC1; p=none; fo=1; rua=mailto:root@f4dev.me"),
 
@@ -27,6 +29,7 @@ D("f4dev.me", REGISTRAR_NONE, DnsProvider(PROVIDER_CLOUDFLARE),
         ]
     }),
 
+    // Domain verificators
     TXT("@", "protonmail-verification=bbdbabda930c1f93a30c9476ddf19b60d5d4c4ad"),
     TXT("@", "google-site-verification=brapgpnqQD1QcBSyGqgeo0I983unWXauUPZp7P6bQxo"),
     TXT("@", "MS=ms48550399"),
