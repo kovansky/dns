5f5f2f882e7eb2177bd4d8f94c2bc2ecf9626223
F4 Developer <skowanski@f4dev.me>
2023-02-16 10:58:50 +0100

    Add `panel.lutharon.f4dev.me`

--- dns/domains/f4dev.me.js
+++ dns/domains/f4dev.me.js
@@ -1,6 +1,7 @@
 D("f4dev.me", REGISTRAR_NONE, DnsProvider(PROVIDER_CLOUDFLARE),
     A("*", AZYMONDIAS),
     A("lutharon", LUTHARON),
+    A("panel.lutharon", LUTHARON),
     // GitHub Pages
     A("@", "192.30.252.153", CF_PROXY_ON),
     A("@", "192.30.252.154", CF_PROXY_ON),
