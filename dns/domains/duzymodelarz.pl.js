53fc7549d5acad062cf479d73a1e80713ff577b9
kovansky <skowanski@f4dev.me>
2024-03-06 18:07:56 +0100

    [Tycidomki,Duzymodelarz] Correct DKIM

--- dns/domains/duzymodelarz.pl.js
+++ dns/domains/duzymodelarz.pl.js
@@ -8,7 +8,7 @@ D("duzymodelarz.pl", REGISTRAR_OVH, DnsProvider(PROVIDER_OVH),
 	MX("@", 0, "mail.duzymodelarz.pl."),
 
 	TXT("_dmarc", "v=DMARC1;  v=DMARC1;p=quarantine;sp=none;adkim=r;aspf=r;pct=100;fo=0;rf=afrf;ri=86400;rua=mailto:dmarc@designcoach.pl;ruf=mailto:dmarc@designcoach.pl"),
-	TXT("default._domainkey", "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtDwbTKX5x271GpZ2pa4Ru6/aS4IaTm/wMmngI94eQ2tnhhMg50Z4m88milVa645WW+dFAT/hn9yE4p5oN/a3QCv/apNmG5wuaccKSvnNIlDGPuOvaQTsyODOj2ThafbsjxDCgw5it3D4n8cCNakM1HOVjrjsUUeWP0h6tIx199VwCxkpSs09gkAnx+WzuAlM9y3PfoZBJJ2uIN8tgo+1T0DBldxy+gXuIDajhf1bOtLJ5tjBicC00P241VLZaXsTTMO1clPeBVMN59svYIboiioX12OkiKiPpc3K+s80LpWfs70iwLcE8OE+mhMYgB7FphS4C1ktPN/S5D74gPzHQQIDAQAB;"),
+	TXT("default._domainkey", "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtdVYz4d9IvjvaYJTcUvQM2kJGh4kAVU3feeGwbs15YCTnfJy8QQW2wey3NOdVqgPQVLT+bawnjUyvY0GEXyMCl2I+z0vINTHORoqdZHrmEzOCf4xEy06q4V5BN7sMr0rO+u2Ej79sVbHcnWKjuqCrHb77jj9YObilGyRnrhYtVVDlBoWqSKRWtVAGIy4GfS+oQlqIHRq0kFgKX0f2shXMgiroyKY5zRPtnA0CLuMKTGXD+I8BGzjBrRmqS6/x+ryNuF04bMd9cCbCRS/U6IzF8rtyNgYdJZFqs3Wl7DSjVlN6Hw6VykFcOQvZvCCOJPpyG2d4J1KfEPznknm5mLkKQIDAQAB;"),
 	SPF_BUILDER({
 		label: "@",
 		parts: [
